3f747fdbbcb24c9d5f4a66f7e8778fe7a46e5943
Taiwo <taiwo_soewu.1995@ymail.com>
2019-06-18 00:01:21 +0100

    feature(develop): updates main css and js

--- UI/js/main.js
+++ UI/js/main.js
@@ -3,16 +3,13 @@ document.addEventListener('DOMContentLoaded', function (event) {
     // Get the modal
     let modal = document.querySelector("#myModal");
 
-    // Get the button that opens the modal
-    let btn = document.querySelector(".myBtn");
-
     // Get the <span> element that closes the modal
     let span = document.querySelector(".close");
 
     // When the user clicks on <span> (x), close the modal
-    span.addEventListener('click', function () {
-        modal.style.display = "none";
-    })
+    // span.addEventListener('click', function () {
+    //     modal.style.display = "none";
+    // })
 
     // When the user clicks anywhere outside of the modal, close it
     window.onclick = function (event) {
@@ -21,8 +18,22 @@ document.addEventListener('DOMContentLoaded', function (event) {
         }
     }
 
+    document.querySelector(".icon").addEventListener('click', function () {
+        myFunction();
+    })
+});
+
+    /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+    const myFunction = () => {
+    var x = document.querySelector("#myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
     const myAlert = (msg) => {
         modal.style.display = "block";
         document.querySelector(".modal-body").textContent = msg;
     }
-});
